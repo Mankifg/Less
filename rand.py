c2094dc94b22ec8d6fbf1570f533234712862b72
Mankifg <83959255+Mankifg@users.noreply.github.com>
2024-08-21 14:35:41 +0200

    reformates

--- rand.py
+++ rand.py
@@ -1,12 +1,14 @@
 import random
 
+
 def random_empty_cell(grid):
     while 1:
         x = random.randint(0, len(grid[0]) - 1)
         y = random.randint(0, len(grid) - 1)
         if grid[y][x] == 0:
             return x, y
-        
+
+
 def random_state_array():
     grid = [[0 for _ in range(6)] for _ in range(6)]
 
@@ -20,28 +22,25 @@ def random_state_array():
 
     return grid
 
-def random_lbp():    
+
+def random_lbp():
     arry = random_state_array()
     print(arry)
     lbp = ""
     c = 0
     for y in arry:
         for sym in y:
-            if sym in ["w","b"]:
+            if sym in ["w", "b"]:
                 if c > 0:
                     lbp += str(c)
                     c = 0
                 lbp += sym
             elif sym == 0:
-                c = c+1
-            
+                c = c + 1
+
         if c > 0:
             lbp += str(c)
             c = 0
         lbp += "/"
-            
-    return lbp.rstrip("/")
-           
-    
-print(random_lbp())
 
+    return lbp.rstrip("/")
