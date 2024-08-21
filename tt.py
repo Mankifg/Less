9b6aa6a08db1484ddadb47cc3a4b892e71bf66fc
Mankifg <83959255+Mankifg@users.noreply.github.com>
2024-08-21 14:33:18 +0200

    removed some stuff

--- tt.py
+++
@@ -1,9 +0,0 @@
-import functions
-
-import time
-
-a = time.time()
-aa_moves = functions.every_absolute_moves()
-b = time.time()
-
-print(f"All moves (84): {len(aa_moves)}\nTime to index all absolute moves: {b-a} sec.")
